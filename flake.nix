e1b28da5c151d27c3c10656b23b3a7fd2af53fc9
Oskar Wickström <oskar@wickstrom.tech>
2024-05-11 07:23:05 +0200

    use flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              git
+              pandoc
+              yq-go
+              fswatch
+              coreutils
+              parallel
+              goaccess
+            ];
+          };
+        };
+      }
+    );
+}
